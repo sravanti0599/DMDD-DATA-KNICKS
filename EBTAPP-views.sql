399e3d188bd6773abd19605fce62e8c6b951b628
saikiran85200 <144652409+saikiran85200@users.noreply.github.com>
2023-11-22 17:27:30 -0500

    views

---
+++ EBTAPP-views.sql
@@ -0,0 +1,122 @@
+
+--Creating views for all the tables
+CREATE OR REPLACE VIEW VIEW_ADMIN
+AS
+SELECT * FROM ADMIN;
+
+CREATE OR REPLACE VIEW VIEW_EBTACCOUNT
+AS
+SELECT * FROM EBTACCOUNT;
+
+CREATE OR REPLACE VIEW VIEW_EBTCARD
+AS
+SELECT * FROM EBTCARD;
+
+CREATE OR REPLACE VIEW VIEW_EBTSCHEDULE
+AS
+SELECT * FROM EBTSCHEDULE;
+
+CREATE OR REPLACE VIEW VIEW_ITEM
+AS
+SELECT * FROM ITEM;
+
+CREATE OR REPLACE VIEW VIEW_EBTAPPLICATION
+AS
+SELECT * FROM EBTAPPLICATION;
+
+CREATE OR REPLACE VIEW VIEW_MERCHANT
+AS
+SELECT * FROM MERCHANT;
+
+CREATE OR REPLACE VIEW VIEW_USERS
+AS
+SELECT * FROM USERS;
+
+CREATE OR REPLACE VIEW VIEW_TRANSACTIONITEMLIST
+AS
+SELECT * FROM TRANSACTIONITEMLIST;
+
+CREATE OR REPLACE VIEW VIEW_TRANSACTIONS
+AS
+SELECT * FROM TRANSACTIONS;
+
+
+--ebtaccount_balance_view
+CREATE OR REPLACE VIEW ebtaccount_balance_view AS
+SELECT
+    ea.accountid,
+    ea.accountnumber,
+    u.firstname,
+    u.ssn,
+    ea.foodbalance,
+    ea.cashbalance,
+    ea.foodbalance + ea.cashbalance AS totalbalance
+FROM
+    VIEW_EBTACCOUNT ea
+JOIN 
+    view_ebtapplication eap
+ON 
+    ea.ebtapplication_applicationid = eap.applicationid
+JOIN 
+    view_users u
+ON
+    eap.users_userid = u.userid;
+
+-- select * from ebtaccount_balance_view;
+   
+--pending_ebt_applications_view
+CREATE OR REPLACE VIEW pending_ebt_applications_view AS
+SELECT
+    e.applicationid,
+    e.benefitprogramname,
+    e.status,
+    e.users_userid,
+    u.firstname,
+    u.email,
+    e.admin_adminid
+FROM
+    view_ebtapplication e
+JOIN view_users u
+ON e.users_userid = u.userid
+WHERE
+    e.status = 'Pending' or e.status = 'PENDING';
+
+
+
+-- select * from view_transactionitemlist;
+-- SELECT * FROM view_transactions;
+-- select * from view_ebtapplication;
+-- select * from pending_ebt_applications_view;
+-- SELECT * FROM view_ebtcard;
+-- select * from users;
+-- select * from view_ebtaccount;
+
+--transaction_summary_view
+CREATE or replace VIEW transaction_summary_view AS
+SELECT
+    t.transactionid,
+    u.firstname,
+    u.email,
+    t.status,
+    t.recorded_date,
+    t.merchant_merchantid,
+    t.ebtcard_cardid,
+    til.quantity,
+    til.item_itemid,
+    i.name AS item_name,
+    i.price AS item_price,
+    (til.quantity * i.price) AS subtotal
+FROM
+    view_transactions t
+JOIN
+    view_transactionitemlist til ON t.transactionid = til.transactions_transactionid
+JOIN
+    view_item i ON til.item_itemid = i.itemid
+JOIN 
+    view_ebtcard ec on ec.cardid = t.ebtcard_cardid
+JOIN 
+    view_ebtapplication eap on eap.applicationid = ec.ebtaccount_accountid
+JOIN 
+    view_users u on u.userid = eap.users_userid;
+
+-- select * from transaction_summary_view;
